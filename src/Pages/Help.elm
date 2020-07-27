5943d307c49b1023085298cc5a07ef12bea567b3
ionif <aionkov@gmail.com>
2020-07-26 21:08:12 -0500

    scaffolded some pages

---
+++ src/Pages/Help.elm
@@ -0,0 +1,35 @@
+module Pages.Help exposing (Params, Model, Msg, page)
+
+import Spa.Document exposing (Document)
+import Spa.Page as Page exposing (Page)
+import Spa.Url as Url exposing (Url)
+
+
+page : Page Params Model Msg
+page =
+    Page.static
+        { view = view
+        }
+
+
+type alias Model =
+    Url Params
+
+
+type alias Msg =
+    Never
+
+
+
+-- VIEW
+
+
+type alias Params =
+    ()
+
+
+view : Url Params -> Document Msg
+view { params } =
+    { title = "Help"
+    , body = []
+    }
---
+++ src/Pages/Settings/Web.elm
@@ -0,0 +1,63 @@
+module Pages.Settings.Web exposing (Params, Model, Msg, page)
+
+import Spa.Document exposing (Document)
+import Spa.Page as Page exposing (Page)
+import Spa.Url as Url exposing (Url)
+
+
+page : Page Params Model Msg
+page =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- INIT
+
+
+type alias Params =
+    ()
+
+
+type alias Model =
+    {}
+
+
+init : Url Params -> ( Model, Cmd Msg )
+init { params } =
+    ( {}, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Settings.Web"
+    , body = []
+    }
