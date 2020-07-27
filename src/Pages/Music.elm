02b4e3d18ec55def7afe9aade2ee89cb186cc990
ionif <aionkov@gmail.com>
2020-07-26 20:43:35 -0500

    added song item elements

--- src/Pages/Music.elm
+++ src/Pages/Music.elm
@@ -4,12 +4,16 @@ import Shared exposing (sendAction, sendActions)
 import Element exposing (..)
 import Element.Border as Border
 import Element.Events
+import Element.Font as Font
+import Element.Input as Input
 import Element.Background as Background
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url as Url exposing (Url)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Svg.Attributes
+import FeatherIcons
 import Request
 import WSDecoder exposing (SongObj, ItemDetails)
 --import Components exposing ()
@@ -82,8 +86,15 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
--- VIEW
 
+featherButton : ( FeatherIcons.Icon, msg ) -> Element msg
+featherButton ( icon, action ) =
+    Input.button [paddingXY 5 3]
+        { onPress = Just action
+        , label = Element.html (icon |> FeatherIcons.withSize 24 |> FeatherIcons.toHtml [ Svg.Attributes.color "lightgrey" ])
+        }
+
+-- VIEW
 
 view : Model -> Document Msg
 view model =
@@ -93,9 +104,19 @@ view model =
         , column [Element.height fill, Element.width fill, scrollbars, clipY, spacingXY 5 7]
             (List.map 
                 (\song -> 
-                    row [Element.width fill, paddingXY 5 5, Border.width 2, Border.rounded 4, mouseOver [ Background.color <| rgb255 86 182 139 ], Element.Events.onDoubleClick (SetCurrentlyPlaying song)] 
-                        [ el [] (Element.text song.label)
-                        , el [alignRight] (Element.text (String.fromInt(song.duration)))
+                    row [Element.width fill, paddingXY 5 5, Background.color (rgb 0.2 0.2 0.2), mouseOver [ Background.color (rgb 0.4 0.4 0.4) ], Element.Events.onDoubleClick (SetCurrentlyPlaying song)] 
+                        [ featherButton (FeatherIcons.play, (SetCurrentlyPlaying song))
+                        , featherButton (FeatherIcons.thumbsUp, (SetCurrentlyPlaying song))
+                        , el [Font.color (Element.rgb 0.8 0.8 0.8)] (Element.text song.label)
+                        , row [alignRight]
+                            (List.map
+                                (\artist -> 
+                                    el [Font.color (Element.rgb 0.8 0.8 0.8), paddingXY 5 0] (Element.text artist)
+                                ) 
+                                song.artist
+                            )
+                        , el [alignRight, Font.color (Element.rgb 0.8 0.8 0.8)] (Element.text (String.fromInt(song.duration)))
+                        , featherButton (FeatherIcons.moreHorizontal, (SetCurrentlyPlaying song))
                         ]
                 ) 
             model.song_list)
