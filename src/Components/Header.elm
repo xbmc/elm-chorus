f8c7ec2c7c027e4edd766c2f80110670a179a1ee
ionif <aionkov@gmail.com>
2020-08-06 10:30:59 -0500

    final fix logo

--- src/Components/Header.elm
+++ src/Components/Header.elm
@@ -1,7 +1,7 @@
 module Components.Header exposing (headerHeight, view)
 
 import Colors
-import Element exposing (Element, alignTop, el, fill, height, image, px, row, text, width)
+import Element exposing (Element, alignTop, el, fill, height, image, px, row, text, width, paddingXY)
 import Element.Background as Background
 import Element.Font as Font exposing (alignRight)
 import Spa.Generated.Route as Route
@@ -17,13 +17,11 @@ view searchChanged =
     row [ width fill, alignTop, height (px headerHeight), Background.color Colors.headerBackground ]
         [ Element.link []
             { label =
-                image [ width (px 50) ]
+                image [ width (px 37), height (px 37), paddingXY 7 0 ]
                     { description = ""
-                    , src = "https://github.com/ionif/elm-chorus/blob/master/logo.png"
+                    , src = "https://raw.githubusercontent.com/ionif/elm-chorus/master/logo.png"
                     }
             , url = Route.Top |> Route.toString
             }
-        , el [ Font.color (Element.rgb 1 1 1) ] (text "Kodi")
-
         -- , Input.search [] { onChange = searchChanged, text = "", placeholder = Just (Input.placeholder [] (Element.text "Search")), label = Input.labelHidden "Search" }
         ]
