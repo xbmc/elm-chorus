871e12ceea12453890d1ec62232fcb8dd23005ef
Priyanshu Jain <priyanshu0405jain@gmail.com>
2021-05-30 13:14:04 +0530

    left-sidebar

--- src/Components/LeftSidebar.elm
+++ src/Components/LeftSidebar.elm
@@ -4,85 +4,129 @@ import Colors
 import Element exposing (..)
 import Element.Background as Background
 import Html.Attributes
+import Element.Events as Events
 import Material.Icons as Filled
 import Material.Icons.Types as MITypes exposing (Coloring(..), Icon)
 import Spa.Generated.Route as Route exposing (Route)
 
+type Model
+    = Normal
+    | Hover
 
-view : Element msg
-view =
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Leave ->
+            Normal
+
+        Enter ->
+            Hover
+
+initialModel : Model
+initialModel =
+    Normal
+
+type Msg
+    = Enter
+    | Leave
+
+view : Model -> Element Msg
+view model =
     column [ height fill, centerX, spacing 20, padding 15, alignLeft, htmlAttribute <| Html.Attributes.style "pointer-events" "all" ]
-        [ musicButton
-        , movieButton
-        , tvshowButton
-        , browserButton
-        , addonsButton
-        , thumbsupButton
-        , playlistButton
-        , settingsButton
-        , helpButton
+        [ musicButton model
+        , movieButton model
+        , tvshowButton model
+        , browserButton model
+        , addonsButton model
+        , thumbsupButton model
+        , playlistButton model
+        , settingsButton model
+        , helpButton model
         ]
 
+--
+-- materialIconLink : ( Icon msg, Route ) -> Element Msg
+-- materialIconLink ( icon, route ) =
+--     row[]
+--     [el
+--         [ Element.mouseOver
+--             [ scale 1.1
+--             , Background.color Colors.brandPrimary
+--             ]
+--         ]
+--     <|
+--         Element.link []
+--             { url = Route.toString route
+--             , label = Element.html (icon 20 (MITypes.Color <| (Colors.fromElementColorToColor <| Colors.greyscaleOuterSpace)))
+--             }
+--       ]
 
-materialIconLink : ( Icon msg, Route ) -> Element msg
-materialIconLink ( icon, route ) =
-    el
-        [ Element.mouseOver
-            [ scale 1.1
-            , Background.color Colors.brandPrimary
-            ]
-        ]
+materialIconLinkNew : Model-> Icon Msg -> Route -> String  -> Element Msg
+materialIconLinkNew  model icon route  name =
+    let
+        temp =
+            case model of
+                Hover ->
+                    [ Element.onRight (Element.text name), scale 1.1, Background.color Colors.brandPrimary]
+
+                Normal ->
+                    []
+    in
+    row[]
+    [el
+        (Events.onMouseEnter Enter
+              :: Events.onMouseLeave Leave
+              :: temp)
     <|
         Element.link []
             { url = Route.toString route
             , label = Element.html (icon 20 (MITypes.Color <| (Colors.fromElementColorToColor <| Colors.greyscaleOuterSpace)))
             }
-
-
+      ]
 
 -- buttons
 
 
-musicButton : Element msg
-musicButton =
-    materialIconLink ( Filled.library_music, Route.Music )
+musicButton : Model -> Element Msg
+musicButton model =
+    materialIconLinkNew model Filled.library_music Route.Music "Music"
 
 
-movieButton : Element msg
-movieButton =
-    materialIconLink ( Filled.movie, Route.Movies__Recent )
+movieButton : Model -> Element Msg
+movieButton model =
+    materialIconLinkNew model Filled.movie Route.Movies__Recent "Movies"
 
 
-tvshowButton : Element msg
-tvshowButton =
-    materialIconLink ( Filled.tv, Route.Tvshows__Recent )
+tvshowButton : Model -> Element Msg
+tvshowButton model =
+    materialIconLinkNew model Filled.tv Route.Tvshows__Recent "TV shows"
 
 
-browserButton : Element msg
-browserButton =
-    materialIconLink ( Filled.list, Route.Browser )
+browserButton : Model -> Element Msg
+browserButton model =
+    materialIconLinkNew model Filled.list Route.Browser "Browser"
 
 
-addonsButton : Element msg
-addonsButton =
-    materialIconLink ( Filled.extension, Route.Addons )
+addonsButton : Model -> Element Msg
+addonsButton model =
+    materialIconLinkNew model Filled.extension Route.Addons "Add-ons"
 
 
-thumbsupButton : Element msg
-thumbsupButton =
-    materialIconLink ( Filled.thumb_up, Route.Thumbsup )
+thumbsupButton : Model -> Element Msg
+thumbsupButton model =
+    materialIconLinkNew model Filled.thumb_up Route.Thumbsup "Thumbs up"
 
 
-playlistButton : Element msg
-playlistButton =
-    materialIconLink ( Filled.assignment, Route.Playlists )
+playlistButton : Model -> Element Msg
+playlistButton model =
+    materialIconLinkNew model Filled.assignment Route.Playlists "Playlists"
 
 
-settingsButton : Element msg
-settingsButton =
-    materialIconLink ( Filled.settings, Route.Settings__Web )
+settingsButton : Model -> Element Msg
+settingsButton model =
+    materialIconLinkNew model Filled.settings Route.Settings__Web "Settings"
 
 
-helpButton : Element msg
-helpButton =
-    materialIconLink ( Filled.help, Route.Help )
+helpButton : Model -> Element Msg
+helpButton model =
+    materialIconLinkNew model Filled.help Route.Help "Help"
